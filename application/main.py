2869b0dff590cc3f7ff00903700451ffd008bcee
Sai Kiran <saikiran@Sais-Air.ht.home>
2023-04-21 11:02:00 -0400

    inittial commit

--- application/main.py
+++ application/main.py
@@ -1,12 +1,6 @@
-import pickle
-from pathlib import Path
-
-import pandas as pd  # pip install pandas openpyxl
-import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 import streamlit_authenticator as stauth  # pip install streamlit-authenticator
 import controller.authenticate as auth
-import json
 import create_user as c
 
 st.set_page_config(page_title="Outlook Login",
